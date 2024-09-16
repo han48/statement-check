c423503772b274c5c90af5a0419a0a3389e8828b
dungdv <dungdv@sabitech.vn>
2024-09-16 11:26:08 +0700

    Add sample data

--- excel2mysql.py
+++ excel2mysql.py
@@ -24,13 +24,14 @@ engine = create_engine(
     f"mysql+mysqlconnector://{DB_USERNAME}:{quote_plus(DB_PASSWORD)}@{DB_HOST}/{DB_DATABASE}")
 date_format = "%d/%m/%Y %H:%M:%S"
 
-directory = 'output'
+directory = 'output/vcb'
 data = list()
 
+rowCount = 0
 datetimeIndex = 0
 amountIndex = 2
 messageIndex = 4
-files = sorted(glob.glob(os.path.join(directory, "data_0*.xlsx")))
+files = sorted(glob.glob(os.path.join(directory, "*.xlsx")))
 
 for filename in files:
     df = pd.read_excel(filename)
@@ -53,31 +54,5 @@ for filename in files:
                     " " + message
         index = index + 1
 
-datetimeIndex = 0
-amountIndex = 2
-messageIndex = 4
-files = sorted(glob.glob(os.path.join(directory, "data_1*.xlsx")))
-
-for filename in files:
-    df = pd.read_excel(filename)
-    total_rows = df.shape[0]
-    index = 0
-    print(f"============={filename}=============")
-    while (index <= total_rows - 1):
-        row = df.iloc[index]
-        amount = str(row.iloc[amountIndex]).replace(".", "")
-        if (amount.isnumeric()):
-            issue_date = datetime.strptime(str(
-                df.iloc[index - 1].iloc[datetimeIndex]) + " 00:00:00", date_format).strftime('%Y-%m-%d %H:%M:%S')
-            message = str(row.iloc[messageIndex])
-            data.append([rowCount, issue_date, int(amount), message])
-            rowCount = rowCount + 1
-        elif (len(data) > 0):
-            message = str(row.iloc[messageIndex])
-            if (message != "nan"):
-                data[len(data) - 1][3] = data[len(data) - 1][3] + \
-                    " " + message
-        index = index + 1
-
-# df = pd.DataFrame(data, columns=['id', 'issue_date', 'amount', 'message'])
-# df.to_sql(table, con=engine, if_exists='replace', index=False)
+df = pd.DataFrame(data, columns=['id', 'issue_date', 'amount', 'message'])
+df.to_sql(table, con=engine, if_exists='replace', index=False)
--- pdf2excel.py
+++ pdf2excel.py
@@ -2,12 +2,11 @@ import tabula
 import PyPDF2
 import os
 
-directory = 'output'
-input = 'input/data2.pdf'
+directory = 'output/vcb'
+input = 'input/data.pdf'
 if not os.path.exists(directory):
     os.makedirs(directory)
 
-start = 0
 total_pages = 0
 with open(input, 'rb') as file:
     reader = PyPDF2.PdfReader(file)
@@ -17,5 +16,5 @@ for page in range(1, total_pages):
     print(f"Export start: {page}/{total_pages - 1}")
     df = tabula.read_pdf(input, pages=page, stream=True)[0]
     df.to_excel(
-        f"{directory}/data_1{str(page + start).zfill(7)}.xlsx", index=False)
+        f"{directory}/data_{str(page).zfill(8)}.xlsx", index=False)
     print(f"Export   end: {page}/{total_pages - 1}")
